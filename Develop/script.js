1336292abeee392dbc66794f449a3d9e68d95942
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-03 13:01:07 +0100

    localstorageLoaddata

--- Develop/script.js
+++ Develop/script.js
@@ -61,5 +61,15 @@ function setCurrentDateAndHour () {
       //set items in local storage.
       localStorage.setItem(time, text);
   }) 
+  //load any saved data from LocalStorage - do this for each hour created.
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
 
   
